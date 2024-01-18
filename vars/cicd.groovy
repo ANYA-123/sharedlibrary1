e6613e46634cff19c9ced6f917a64f4b133a380a
ANYA-123 <153912007+ANYA-123@users.noreply.github.com>
2024-01-18 10:02:27 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,6 +1,6 @@
 def gitDownload(repo)
 {
-  git "https://github.com/ANYA-123/${repo}.git"
+   git "https://github.com/ANYA-123/${repo}.git"
 }
 
 def mavenBuild()
@@ -12,4 +12,8 @@ def deployTomcat(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
- 
+
+def runSelenium(jobname)
+{
+  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
